32ec7bba8b1ba37e85e263fc7ccf5436fd964086
Crowdbotics <team@crowdbotics.com>
2021-10-28 16:24:52 +0000

    Update models

--- backend/home/migrations/0002_userdetails.py
+++ backend/home/migrations/0002_userdetails.py
@@ -8,18 +8,26 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('home', '0001_load_initial_data'),
+        ("home", "0001_load_initial_data"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Userdetails',
+            name="Userdetails",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('user_id', models.BigIntegerField()),
-                ('first_name', models.CharField(max_length=256)),
-                ('last_name', models.CharField(max_length=256)),
-                ('address', models.TextField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("user_id", models.BigIntegerField()),
+                ("first_name", models.CharField(max_length=256)),
+                ("last_name", models.CharField(max_length=256)),
+                ("address", models.TextField()),
             ],
         ),
     ]
